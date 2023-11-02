9d0018a146ee6fab7c3e59e274d9ef9616c10647
itamim-coder <itamim12202@gmail.com>
2023-11-03 04:47:47 +0600

    config

--- src/helpers/config/envConfig.ts
+++ src/helpers/config/envConfig.ts
@@ -1,8 +1,8 @@
 export const getBaseUrl = ():string => {
     
-    return process.env.NEXT_PUBLIC_API_BASE_URL || " https://traverse-v2.vercel.app"
+    return process.env.NEXT_PUBLIC_API_BASE_URL || "https://traverse-v2.vercel.app/api/v1"
 }
 
-//  https://traverse-v2.vercel.app
+//  https://traverse-v2.vercel.app/api/v1
 
 // http://localhost:3030/api/v1
