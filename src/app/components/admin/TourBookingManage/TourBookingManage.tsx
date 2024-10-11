6a0abd3456e45ddb3b11a02f416fb9cf1bc83d46
itamim-coder <itamim12202@gmail.com>
2024-10-11 20:33:35 +0600

    [UPDATED] Booking Data

--- src/app/components/admin/TourBookingManage/TourBookingManage.tsx
+++ src/app/components/admin/TourBookingManage/TourBookingManage.tsx
@@ -4,7 +4,6 @@ import Alert from "@/app/components/ui/Alert/alert";
 import Pagination from "@/app/components/ui/Pagination/Pagination";
 import { useUserHotelQuery, useUserTourQuery } from "@/redux/api/bookingApi";
 
-
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
@@ -21,7 +20,7 @@ const TourBookingManage = () => {
   query["sortBy"] = sortBy;
   query["sortOrder"] = sortOrder;
   const { data: tourBooking, isLoading } = useUserTourQuery({ ...query });
-//   const [deleteLocation] = useDeleteLocationMutation();
+  //   const [deleteLocation] = useDeleteLocationMutation();
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }, [page]);
@@ -31,25 +30,25 @@ const TourBookingManage = () => {
   const meta = tourBooking?.meta;
 
   const data = tourBooking;
-
+  console.log("tourBooking", data);
   // Assuming tourBooking.meta.totalPages exists
   const handlePageChange = (pageNumber: number) => {
     setPage(pageNumber + 1); // Pagination starts from 0, but page state starts from 1
   };
 
-//   const handleDelete = async (id: string) => {
-//     toast.loading("Deleting....");
+  //   const handleDelete = async (id: string) => {
+  //     toast.loading("Deleting....");
 
-//     try {
-//       const res = await deleteLocation(id).unwrap();
-//       console.log(res);
-//       if (res.id) {
-//         toast.success("Delete Successfully");
-//       }
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   };
+  //     try {
+  //       const res = await deleteLocation(id).unwrap();
+  //       console.log(res);
+  //       if (res.id) {
+  //         toast.success("Delete Successfully");
+  //       }
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
 
   return (
     <div className="m-5">
@@ -67,13 +66,22 @@ const TourBookingManage = () => {
                 Serial
               </th>
               <th className="py-4 px-6 text-base font-medium text-left  border-b">
-                Location
+                Customer
               </th>
               <th className="py-4 px-6 text-base font-medium text-left border-b ">
-                Location Name
+                Phone
               </th>
               <th className="py-4 px-6 text-base font-medium text-left border-b ">
-                Featured
+                Package Name
+              </th>
+              <th className="py-4 px-6 text-base font-medium text-left border-b ">
+                Booking Status
+              </th>
+              <th className="py-4 px-6 text-base font-medium text-left border-b ">
+                Pay
+              </th>
+              <th className="py-4 px-6 text-base font-medium text-left border-b ">
+                Payment Status
               </th>
               <th className="py-4 px-6 text-base font-medium border-b text-end">
                 Action
@@ -87,29 +95,35 @@ const TourBookingManage = () => {
                   <td className="py-4 px-6 border-b text-base font-medium font-medium">
                     {index + 1}
                   </td>
-                  <td className="py-4 px-4 flex justify-start">
-                    <img
-                      src={data?.image}
-                      alt="table navigate ui"
-                      className="h-16 w-16 object-cover bg-gray-300"
-                    />
+
+                  <td className="py-4 px-6 border-b text-base font-medium">
+                    {data?.customer_name}
                   </td>
                   <td className="py-4 px-6 border-b text-base font-medium">
-                    {data?.name}
+                    {data?.phone}
                   </td>
                   <td className="py-4 px-6 border-b text-base font-medium">
-                    {data?.featured ? "Yes" : "No"}
+                    {data?.tourBooks?.tourInfo?.title}
+                  </td>
+                  <td className="py-4 px-6 border-b text-base font-medium">
+                    {data?.status}
+                  </td>
+                  <td className="py-4 px-6 border-b text-base font-medium">
+                    {data?.tourBooks?.totalAmount}
+                  </td>
+                  <td className="py-4 px-6 border-b text-base font-medium">
+                    {data?.status}
                   </td>
                   <td className="py-4 px-6  border-b text-end">
                     <button className="bg-blue-500 mx-1 hover:scale-110 scale-100 transition-all duration-100 text-white py-2 px-4 rounded-md">
                       Details
                     </button>
-                    <button
+                    {/* <button
                       onClick={() => handleDelete(data.id)}
                       className="bg-red-500 mx-1 hover:scale-110 scale-100 transition-all duration-100 text-white py-2 px-4 rounded-md"
                     >
                       Delete
-                    </button>
+                    </button> */}
                   </td>
                 </tr>
               </>
